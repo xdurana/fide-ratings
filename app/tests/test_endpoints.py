be03ad20cfcaa529508416942cb8f20d85622e5b
xdurana <xadual@gmail.com>
2018-12-29 07:24:09 +0100

    Host url

--- app/tests/test_endpoints.py
+++ app/tests/test_endpoints.py
@@ -1,6 +1,6 @@
 import requests
 
 def test_api():
-    r = requests.get('http://localhost:5000/api')
+    r = requests.get('http://127.0.0.1:5000/api/')
     assert r.status_code == 200
     assert True
